9dca490839d8e4555819f60a5ebecbe7b0436400
pinair <riccardo.sibani@gmail.com>
2015-09-28 19:43:41 +0200

    Adding user.php class

--- Classes/user.php
+++ Classes/user.php
@@ -1,1 +1,29 @@
 <?php
+
+/**
+ *  
+ * @author riccardosibani
+ * 
+ * user.php
+ * USER CLASS
+ * 
+ * This class allow to create user object in order to represent every single user and login function
+ */
+
+Class User{
+	
+	public $id;
+	public $name;
+	public $surname;
+	public $mail;
+	public $password;
+	public $class;
+	public $deleted;
+	
+	function __construct($name, $surname, $mail, $password, $class){
+		$this->name = $name;
+		$this->surname = $surname;
+		$this->mail = $mail;
+		$this->password = $password;
+	}
+}

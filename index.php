5c54fc9f69de2a91a4bfba53b25f195e93f27100
Alberto Anceschi <anceschialberto@me.com>
2015-09-29 00:02:37 +0200

    changed post credential name

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <h1>Log in</h1>
     <br>
     <form action="Functions/LoginFunction.php" method="post">
-        <input type="text" name="user" placeholder="Username">
+        <input type="text" name="mail" placeholder="Username">
         <input type="password" name="pass" placeholder="Password">
         <input type="submit" name="login" class="login login-submit" value="Log in">
     </form>

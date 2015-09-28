5843c724f4ce15258b48f007b5e3b2caf80e9891
pinair <riccardo.sibani@gmail.com>
2015-09-29 01:05:47 +0200

    Blank

--- main.php
+++ main.php
@@ -1,12 +1,1 @@
-<?php
-session_start();
-if(empty($_SESSION['login'])){
-    if(isset($_COOKIES['urka'])){
-        $_SESSION['login_id'] = $_COOKIES['urka'];
-    } else{
-        if($_GET['content']!="login" && $_GET['content']!="registration"){
-            header('Location: index.php');
-        }
-    }
-}
-?>
+

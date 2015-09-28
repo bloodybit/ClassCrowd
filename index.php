1a6011b09906a7eb9057cb3eb84335f36c3719fc
Alberto Anceschi <anceschialberto@me.com>
2015-09-29 01:56:48 +0200

    created header proto @ index.php

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="ISO-8859-1">
     <title>ClassCrowd</title>
-    <link rel="stylesheet" type="text/css" href="css/960_12_col.css" />
-    <link type="text/css" href="css/indexstyle.css" />
+    <link rel="stylesheet" type="text/css" href="css/indexstyle.css" />
 </head>
 <body>
-<div class="login-card">
-    <h1>Log in</h1>
-    <br>
-    <form action="Functions/LoginFunction.php" method="post">
-        <input type="text" name="mail" placeholder="Username">
-        <input type="password" name="pass" placeholder="Password">
-        <input type="submit" name="login" class="login login-submit" value="Log in">
-    </form>
-    <!--
-    <form action="registerpage.php">
-        <input type="submit" class="login login-submit" value="Register">
-    </form>
-    -->
-    <div class="login-help">
-        <?php if(isset($_COOKIE["user"]))
-        {
-            setcookie ("User", "", time() - 3600);
-            echo("No user named ".$_COOKIE["user"]);
-        }?>
-    </div>
+<div class="wrapper">
+    <header>
+        <h1>ClassCrowd</h1>
+        <nav>
+            <ul>
+                <li><a href="" class="current">home</a></li>
+                <li><a href="">about</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+        </nav>
+        <form class="login-card" action="Functions/LoginFunction.php" method="post">
+            <input type="text" name="mail" placeholder="Username">
+            <input type="password" name="pass" placeholder="Password">
+            <input type="submit" name="login" class="login login-submit" value="Log in">
+        </form>
+    </header>
+    <section class="welcome">
+        <article></article>
+    </section>
+    <aside>
+        <section class="register">
+            <!--
+            <form class="register-card" action="" method="post">
+                <input type="text" name="mail" placeholder="Username">
+                <input type="password" name="pass" placeholder="Password">
+                <input type="submit" name="login" class="login login-submit" value="Log in">
+            </form>
+            -->
+        </section>
+    </aside>
 </div>
 </body>
 </html>

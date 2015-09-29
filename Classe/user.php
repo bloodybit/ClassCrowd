d1ae4be18978bdc36e21eb12613318e6b3e34669
pinair <riccardo.sibani@gmail.com>
2015-09-30 00:55:14 +0200

    Done sidebar Class - Subject - Lesson Done Empty space in Content Done Lesson in content
    What else do you want bitches?? Al faz tot me

---
+++ Classe/user.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ *  
+ * @author riccardosibani
+ * 
+ * user.php
+ * USER CLASS
+ *
+ * This class allow to create user object in order to represent every single user and login function
+ */
+
+class User{
+	
+	public $id;
+	public $name;
+	public $surname;
+	public $mail;
+	public $password;
+	public $class;
+	public $deleted;
+
+	function __construct($input = false){
+		if (is_array($input)) {
+			foreach ($input as $key => $val) {
+				// Note the $key instead of key.
+				// This will give the value in $key instead of 'key' itself
+				$this->$key = $val;
+			}
+		}
+	}
+
+	function fullConstruct($name, $surname, $mail, $password, $class){
+		$this->name = $name;
+		$this->surname = $surname;
+		$this->mail = $mail;
+		$this->password = $password;
+		$this->class = $class;
+		$this->deleted = false;
+	}
+
+	function withIdAndDeleted($id, $name, $surname, $mail, $password, $class, $deleted){
+		$this->fullConstruct($name, $surname, $mail, $password, $class);
+		$this->id = $id;
+		$this->deleted = $deleted;
+	}
+
+	function getId(){
+		return $this->id;
+	}
+
+	function setId($id){
+		$this->id = $id;
+	}
+
+	function getName(){
+		return $this->name;
+	}
+
+	function setName($name){
+		$this->name = $name;
+	}
+
+	function getSurname(){
+		return $this->surname;
+	}
+
+	function setSurname($surname){
+		$this->surname = $surname;
+	}
+
+	function getMail(){
+		return $this->mail;
+	}
+
+	function setMail($mail){
+		$this->mail = $mail;
+	}
+
+	function getPassword(){
+		return $this->password;
+	}
+
+	function setPassword($password){
+		$this->password = $password;
+	}
+
+	function getClass(){
+		return $this->class;
+	}
+
+	function setClass($class){
+		$this->class = $class;
+	}
+
+	function isDeleted(){
+		return $this->deleted;
+	}
+
+	function setDelete(){
+		$this->delete = true;
+	}
+}

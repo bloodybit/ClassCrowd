b94c1227b509356451202c1b2b609b7e61117ef1
Bakufreak <anderspreben@gmail.com>
2015-09-29 13:52:31 +0200

    Updated index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-    session_start();
+    /*session_start();
     if($_COOKIE['id']==$_SESSION['id']){
         header("Location:main.php");
-    }
+    }*/
 ?>
 
 <!DOCTYPE html>
@@ -10,49 +10,62 @@
 <head>
     <title>ClassCrowd</title>
     <link rel="stylesheet" type="text/css" href="css/indexstyle.css" />
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<div class="wrapper">
-    <header>
-        <h1>ClassCrowd</h1>
-        <nav>
-            <ul>
-                <li><a href="" class="current">home</a></li>
-                <li><a href="">about</a></li>
-                <li><a href="">contact</a></li>
-            </ul>
-        </nav>
-        <form class="login-card" action="Functions/LoginFunction.php" method="post">
-            <input type="text" name="mail" placeholder="Username">
-            <input type="password" name="pass" placeholder="Password">
-            <input type="submit" name="login" class="login login-submit" value="Log in">
-        </form>
-        <div id="message">
-            <?php
-            session_start();
 
-            if(isset($_SESSION['message'])){
-                echo("<h4>".$_SESSION['message']."</h4>");
-                unset($_SESSION['message']);
-            }
-            ?>
-        </div>
-    </header>
+
+<aside>
+	<!--
+	<div id="avatar">
+		<img id="avatar-pic" src="http://i.imgur.com/iAmtHlm.png">
+		<div id="avatar-txt">
+			<p class="cut-text"><b>asdf</b></p>
+			<p class="cut-text">5915web1ei</p>
+		</div>
+	</div>
+	-->
+
+	<section class="login">	
+	<h2>Login</h2>
+	<div id="message">
+		<?php
+			session_start();
+
+			if(isset($_SESSION['message'])){
+				echo("<p>".$_SESSION['message']."</p>");
+				unset($_SESSION['message']);
+			}
+			?>
+</div>
+	<form class="login-card" action="Functions/LoginFunction.php" method="post">
+		<input type="text" name="mail" class="input-field" placeholder="Email">
+		<input type="password" name="pass" class="input-field" placeholder="Password">
+		<input type="submit" name="login" class="login login-submit" value="Log in">
+	</form>
+	</section>
+	
+	<section class="register">
+		<h2>Register</h2>
+		<input type="submit" name="register" class="login login-submit" value="Register">
+	</section>
+	
+</aside>
+
+
+<div class="wrapper">
+
+
+        
+            </header>
     <section class="welcome">
         <article></article>
     </section>
-    <aside>
-        <section class="register">
-            <!--
-            <form class="register-card" action="" method="post">
-                <input type="text" name="mail" placeholder="Username">
-                <input type="password" name="pass" placeholder="Password">
-                <input type="submit" name="login" class="login login-submit" value="Log in">
-            </form>
-            -->
-        </section>
-    </aside>
+    <div>
+
+    </div>
 </div>
+
 </body>
 </html>
 

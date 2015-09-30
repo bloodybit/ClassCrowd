d33547368b217daa00e5371f9004c0e8f1c30603
pinair <riccardo.sibani@gmail.com>
2015-09-30 14:52:47 +0200

    Added getUserById() STATIC

--- Classe/photo.php
+++ Classe/photo.php
@@ -14,5 +14,44 @@ class Photo{
     public $date;
     public $deleted;
 
+    function __construct($input = false){
+        if (is_array($input)) {
+            foreach ($input as $key => $val) {
+                // Note the $key instead of key.
+                // This will give the value in $key instead of 'key' itself
+                $this->$key = $val;
+            }
+        }
+    }
+
+    function getId(){
+        return $this->id;
+    }
+
+    function getLectureId(){
+        return $this->lecture_id;
+    }
+
+    function setLectureId(){
+        $this->lecture_id = lecture_id;
+    }
+
+    function getPath(){
+        return $this->path;
+    }
+
+    function setPath($path){
+        $this->path = $path;
+    }
+
+    function getBulletId(){
+        return $this->bullet_id;
+    }
+
+    function setBulletId($bullet_id){
+        $this->bullet_id = $bullet_id;
+    }
+
+    function
 
 }

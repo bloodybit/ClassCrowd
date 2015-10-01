5cdf5753fa1e7651c13ceeec81b3dcbb0504d278
Bakufreak <anderspreben@gmail.com>
2015-10-01 14:49:59 +0200

    minor change

--- SidebarCode/lessons.php
+++ SidebarCode/lessons.php
@@ -22,6 +22,4 @@ foreach($lessonsList as $lesson){
 ?>
 <br><br>
 
-<!--<a href="main.php?sidebar=subject&class_id="><i class="fa fa-arrow-circle-o-left"></i> Back to Subjects</a> 
-<br> -->
 <a href="main.php?sidebar=class"><i class="fa fa-arrow-circle-left"></i> Back to Classes</a>

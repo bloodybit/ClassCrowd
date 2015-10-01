94223615e23395d59fb5739b42739ec14226fe1a
viesii <viestursbein@gmail.com>
2015-10-01 12:49:17 +0200

    styling

--- ContentCode/empty.php
+++ ContentCode/empty.php
@@ -1,1 +1,28 @@
-<h1>Empty</h1>
+<h1>Welcome to ClassCrowd</h1>
+
+
+<p>
+	
+<h3>Hi
+		<?php  
+			$user = User::getUserById($_COOKIE['id']);
+			echo $user->name . ","; 
+		?>
+</h3>
+
+<p>In order to start, you have to choose a class in the left sidebar!</p>
+	
+	<ul id="empty-list">
+	<img id="arrow-left" src="images/arrow-left.png">
+
+	<li><span>On ClassCrowd you are able to view any <span class="special-char">class</span> you wish</span></li>
+	<li><span>In order to add/edit content of the <span class="special-char">class</span> you have to belong to it.</span></li>
+	<li><span>Obviosly you can be only be in one <span class="special-char">class</span> at the same time... :) </span></li>
+	<li><span>Your <span class="special-char">class</span> is ($userclassGOESHERE)</span></li>
+	<li><span>In <span class="special-char">class</span> you can view/create <span class="special-char">Subjects</span></span></li>
+	<li><span>In <span class="special-char">Subject</span> you can view/create/edit lesson <span class="special-char">Notes</span></span></li>
+	</ul>
+ 
+
+
+</p>

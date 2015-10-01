d3a9cbe1114bc16b3b25d404852721a32c9f24e2
viesii <viestursbein@gmail.com>
2015-10-01 14:48:28 +0200

    suject changes

--- SidebarCode/subject.php
+++ SidebarCode/subject.php
@@ -1,11 +1,33 @@
-<h2>Subjects</h2>
+ <?php $connection = Database::getConnection();
+                                    $query = "SELECT class FROM class WHERE id=".$_GET['class_id'];
+
+                                
+                                    $result_obj = $connection->query($query);
+                                    
+                                        //I COULD USE A FOR AND IT WOULD BE BETTER
+                                        //BUT IT DOESN'T WORK AND I HAVE NO TIME TO
+                                        //FIND THE PROBLEM :)
+                                        $i=0;
+                                        while($result = $result_obj->fetch_array(MYSQLI_ASSOC)){
+                                            $class = $result;
+                                            $i++;
+                                        }
+                                   
+?>
+
+<h2><?php  echo $class['class'] . ' / '; ?><span id="subject-title">Subjects</span></h2>
 <br>
 <?php
     $subjects = Subject::getSubjectByClass($_GET['class_id']);
 
+ if (!empty($subjects)) {
     foreach($subjects as $sub){
         echo('<div class="sidebar-link"><a href="main.php?sidebar=subject&class_id='.$_GET['class_id'].'&subject_id='.$sub->getId().'&content=lessons">'.$sub->getSubject().'</a></div>');
     }
+ }
+ else{
+ 	echo "No subjects created yet!";
+ }
 //main.php?sidebar=subject&subject_id='.$sub->getId().'&content=lessons
 ?>
 

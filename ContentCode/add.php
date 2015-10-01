8148105a936063b423998f2dd19a9c9cb4ba2dc6
Bakufreak <anderspreben@gmail.com>
2015-10-01 22:22:59 +0200

    Add styling done

--- ContentCode/add.php
+++ ContentCode/add.php
@@ -28,33 +28,39 @@ $_SESSION['subject_id'] = $_GET['subject_id'];
 </div>
 
 
-
+<div class="bg1"></div>
 
 <br><br><br>
 
-<form enctype="multipart/form-data" action="Functions/AddContent.php" method="post">
+<form class="form-wrap" enctype="multipart/form-data" action="Functions/AddContent.php" method="post">
     
     <div class="add-1">
-	    <h2>1. Pick Lesson Hightligh</h2>
+    	<br>
+	    <h2>1. Pick Lesson Highlight</h2>
+	    <br>
 	    <?php
 	        foreach($bullets as $bullet){
 	            echo '<label><input type="radio" name="bullet" value="'.$bullet->getId().'">'.$bullet->getBullet().'</label><br>';
 	        }
 	    ?>
-	    <label><input type="radio" name="bullet" value="jolly">Add new Lesson Hightlight:<br><input type="text" name="newBullet"></label> <br>
+	    <label><input class="input-addnew" type="radio" name="bullet" value="jolly">...or add new Lesson Hightlight:</label><br><input class="newbullet" type="text" name="newBullet">
+	    <br><br><br><br><br><br>
 	</div>
 
     <div class="add-2">
-		<h2>2. Add notes to Lesson</h2>
+		<br>
+		<h2>2. Add notes to chosen Lesson Highlight</h2>
+		<br>
 	    <h3>Add Text</h3>
 	    <textarea name="text" rows="6" cols="40"></textarea>
-	
+		<br><br>
 	    <h3>Add Code</h3>
 	    <textarea name="code" rows="6" cols="40"></textarea>
-	
-	    <h3>Add Img</h3>
+		<br><br>
+	    <h3>Add Image</h3>
 	    <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
 	    Send this Image: <input name="photo" type="file" />
+	    <br><br><br><br><br><br>
     </div>
 
     <div class="add-3">
